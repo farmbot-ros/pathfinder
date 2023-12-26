4718cb16a09b3d06d63ce2fdb8461238a74aa008
Trim Bresilla <trim.bresilla@gmail.com>
2023-12-26 16:55:06 +0100

    feat: restructure path server and navigation classes
    - Modify `config/params.yaml`:
       - Add `path_server` section with parameters `name`, `odom_topic`, and `fix_topic`
    - Modify `launch/navigation.launch.py`:
       - Remove unused imports
       - Add launch arguments `namespace` and `double_antenna`
       - Modify `generate_launch_description` function to include new launch arguments
    - Modify `src/path_server.cpp`:
       - Rename class `Modifier` to `Locator`
       - Modify constructor to initialize variables `name` and `topic_prefix_param`
       - Modify `setValues` function to subscribe to `fix_sub_` and `odom_sub_`
       - Rename function `changeValues` to `change_values`
       - Add new functions `sync_callback` and `path_timer_callback`
    - Modify `src/path_server.cpp`:
       - Rename class `Reader` to `Navigator`
       - Modify constructor to initialize variables `name` and `topic_prefix_param`
       - Modify `setValues` function to subscribe to `fix_sub_` and `odom_sub_`
       - Rename function `readValues` to `read_values`

--- src/path_server.cpp
+++ src/path_server.cpp
@@ -13,24 +13,48 @@
 #include <mutex>
 #include <string>
 
-class Modifier : public rclcpp::Node {
+class Locator : public rclcpp::Node {
     private:
         int* numberPtr;
+        bool* inited_waypoints;
         geometry_msgs::msg::Pose* pose;
         sensor_msgs::msg::NavSatFix* gps;
         std::vector<geometry_msgs::msg::PoseStamped>* points;
         nav_msgs::msg::Path* path;
 
+        message_filters::Subscriber<sensor_msgs::msg::NavSatFix> fix_sub_;
+        message_filters::Subscriber<nav_msgs::msg::Odometry> odom_sub_;
+        std::shared_ptr<message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::NavSatFix, nav_msgs::msg::Odometry>>> sync_;
+
+        rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub;
+
         std::mutex mtx;
         rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::TimerBase::SharedPtr path_timer;
 
     public:
-        Modifier() : Node("modifier"){
-            timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&Modifier::changeValues, this));
+        Locator() : Node("path_server"){
+            std::string name = "path_server";
+            std::string topic_prefix_param = "/fb";
+            try {
+                std::string name = this->get_parameter("name").as_string(); 
+                std::string topic_prefix_param = this->get_parameter("topic_prefix").as_string();
+            } catch (...) {
+                RCLCPP_INFO(this->get_logger(), "No parameters found, using default values");
+            }
+
+            fix_sub_.subscribe(this, topic_prefix_param + "/loc/fix");
+            odom_sub_.subscribe(this, topic_prefix_param + "/loc/odom");
+            sync_ = std::make_shared<message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::NavSatFix, nav_msgs::msg::Odometry>>>(10);
+            sync_->connectInput(fix_sub_, odom_sub_);
+            sync_->registerCallback(std::bind(&Locator::sync_callback, this, std::placeholders::_1, std::placeholders::_2));
+            timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&Locator::change_values, this));
+            path_timer = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&Locator::path_timer_callback, this));
         }
 
-        void setValues(
+        void set_values(
             int* numPtr,
+            bool* inited_waypointsPtr,
             geometry_msgs::msg::Pose* posePtr,
             sensor_msgs::msg::NavSatFix* gpsPtr,
             std::vector<geometry_msgs::msg::PoseStamped>* pointsPtr,
@@ -38,24 +62,60 @@ class Modifier : public rclcpp::Node {
         ) {
             RCLCPP_INFO(this->get_logger(), "Setting values");
             numberPtr = numPtr;
+            inited_waypoints = inited_waypointsPtr;
             pose = posePtr;
             gps = gpsPtr;
             points = pointsPtr;
             path = pathPtr;
         }
 
-        void changeValues() {
+        void change_values() {
             std::lock_guard<std::mutex> lock(mtx);
             if (numberPtr) {
                 (*numberPtr) += 1;
                 RCLCPP_INFO(this->get_logger(), "Values changed to %d", *numberPtr);
             }
         }
+        void sync_callback(const sensor_msgs::msg::NavSatFix::ConstSharedPtr& fix, const nav_msgs::msg::Odometry::ConstSharedPtr& odom) {
+            std::lock_guard<std::mutex> lock(mtx);
+            if (gps) {
+                gps->latitude = fix->latitude;
+                gps->longitude = fix->longitude;
+                gps->altitude = fix->altitude;
+                // RCLCPP_INFO(this->get_logger(), "GPS changed");
+            }
+            if (pose) {
+                pose->position.x = odom->pose.pose.position.x;
+                pose->position.y = odom->pose.pose.position.y;
+                pose->position.z = odom->pose.pose.position.z;
+                pose->orientation.x = odom->pose.pose.orientation.x;
+                pose->orientation.y = odom->pose.pose.orientation.y;
+                pose->orientation.z = odom->pose.pose.orientation.z;
+                pose->orientation.w = odom->pose.pose.orientation.w;
+                // RCLCPP_INFO(this->get_logger(), "Pose changed");
+            }
+        }
+        void path_timer_callback(){
+            std::lock_guard<std::mutex> lock(mtx);
+            path->header.stamp = this->now();
+            path->header.frame_id = "map";
+            if (points && inited_waypoints){
+                for (auto &i : *points){
+                    geometry_msgs::msg::PoseStamped pose;
+                    pose.header = path->header;
+                    pose.pose.position.x = i.pose.position.x;
+                    pose.pose.position.y = i.pose.position.y;
+                    path->poses.push_back(pose);
+                }
+            }
+            path_pub->publish(*path);
+        }
 };
 
-class Reader : public rclcpp::Node {
+class Navigator : public rclcpp::Node {
     private:
         int* numberPtr;
+        bool* inited_waypoints;
         geometry_msgs::msg::Pose* pose;
         sensor_msgs::msg::NavSatFix* gps;
         std::vector<geometry_msgs::msg::PoseStamped>* points;
@@ -65,12 +125,13 @@ class Reader : public rclcpp::Node {
         rclcpp::TimerBase::SharedPtr timer_;
 
     public:
-        Reader() : Node("reader"){
-            timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&Reader::readValues, this));
+        Navigator() : Node("path_server_2"){
+            timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&Navigator::readValues, this));
         }
 
-        void setValues(
+        void set_values(
             int* numPtr,
+            bool* inited_waypointsPtr,
             geometry_msgs::msg::Pose* posePtr,
             sensor_msgs::msg::NavSatFix* gpsPtr,
             std::vector<geometry_msgs::msg::PoseStamped>* pointsPtr,
@@ -78,6 +139,7 @@ class Reader : public rclcpp::Node {
         ) {
             RCLCPP_INFO(this->get_logger(), "Setting values");
             numberPtr = numPtr;
+            inited_waypoints = inited_waypointsPtr;
             pose = posePtr;
             gps = gpsPtr;
             points = pointsPtr;
@@ -96,15 +158,16 @@ int main(int argc, char * argv[]){
     rclcpp::init(argc, argv);
 
     int five = 0;
+    bool inited_waypoints = false;
     geometry_msgs::msg::Pose pose;
     sensor_msgs::msg::NavSatFix gps;
     std::vector<geometry_msgs::msg::PoseStamped> points;
     nav_msgs::msg::Path path;
 
-    auto modifier = std::make_shared<Modifier>();
-    modifier->setValues(&five, &pose, &gps, &points, &path);
-    auto reader = std::make_shared<Reader>();
-    reader->setValues(&five, &pose, &gps, &points, &path);
+    auto modifier = std::make_shared<Locator>();
+    modifier->set_values(&five, &inited_waypoints, &pose, &gps, &points, &path);
+    auto reader = std::make_shared<Navigator>();
+    reader->set_values(&five, &inited_waypoints, &pose, &gps, &points, &path);
 
     rclcpp::executors::MultiThreadedExecutor executor;
     executor.add_node(modifier);
