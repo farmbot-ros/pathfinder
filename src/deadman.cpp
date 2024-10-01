05ca031b631fa9571c6efa3e765663115d17da04
Trim Bresilla <trim.bresilla@gmail.com>
2024-10-01 10:23:08 +0200

    refactor: refactor and update `Deadman` class functionality
    - Rename the parameter `dead_man_switch` to `deadman` in the `params.yaml` file
    - Add inclusions for `sensor_msgs` in `deadman.cpp`
    - Refactor the `Deadman` constructor in `deadman.cpp` to handle parameters dynamically
    - Implement callbacks for timer and joystick in the `Deadman` class in `deadman.cpp`

--- src/deadman.cpp
+++ src/deadman.cpp
@@ -1,9 +1,53 @@
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joy.hpp>
 
 class Deadman : public rclcpp::Node {
+    private:
+        std::string name;
+        std::string topic_prefix_param;
+        std::string joy_topic;
+        int deadman_button;
+
+        rclcpp::TimerBase::SharedPtr deadman_timer;
+        rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub;
+
     public:
-        Deadman(): Node("deadman") {
-            RCLCPP_INFO(this->get_logger(), "Deadman node started");
+        Deadman(): Node("deadman",
+            rclcpp::NodeOptions()
+            .allow_undeclared_parameters(true)
+            .automatically_declare_parameters_from_overrides(true)
+        ) {
+            try {
+                name = this->get_parameter("name").as_string(); 
+                topic_prefix_param = this->get_parameter("topic_prefix").as_string();
+            } catch (...) {
+                name = "deadman";
+                topic_prefix_param = "/fb";
+            }
+
+            try{
+                rclcpp::Parameter joy_param = this->get_parameter("joy_topic");
+                joy_topic = joy_param.as_string();
+                rclcpp::Parameter deadman_button_param = this->get_parameter("deadman_button");
+                deadman_button = deadman_button_param.as_int();
+            } catch(const std::exception& e) {
+                RCLCPP_WARN(this->get_logger(), "Could not find parameters: joy_topic, deadman_button, using defaults");
+                joy_topic = topic_prefix_param + "/joy";
+                deadman_button = 5;
+            }
+
+            deadman_timer = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&Deadman::deadman_timer_callback, this));
+            joy_sub = this->create_subscription<sensor_msgs::msg::Joy>(joy_topic, 10, std::bind(&Deadman::joy_callback, this, std::placeholders::_1));
+        }
+    private:
+        void deadman_timer_callback(){
+            RCLCPP_INFO(this->get_logger(), "Deadman timer");
+        }
+
+        void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg){
+            if (msg->buttons[deadman_button] == 1) {
+                RCLCPP_INFO(this->get_logger(), "Deadman pressed");
+            }
         }
 };
 
